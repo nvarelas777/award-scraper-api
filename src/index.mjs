58acdf885aac1a0600ae28a0e62afddd88c236a3
DESKTOP-942RFIF\HBVik <takethisjaguar@gmail.com>
2023-02-18 15:46:17 -0800

    test2

--- src/index.mjs
+++ src/index.mjs
@@ -12,14 +12,14 @@ export const lambdaHandler = async (event, context) => {
     };
   }
   try {
-    const client = new Client(connectionString);
-    await client.connect();
-    var result = await client.query("select * from testtable t");
-    result.rows.forEach((x) => {
-      console.log(x);
-    });
-    //console.log(result);
-    await client.end();
+    // const client = new Client(connectionString);
+    // await client.connect();
+    // var result = await client.query("select * from testtable t");
+    // result.rows.forEach((x) => {
+    //   console.log(x);
+    // });
+    // //console.log(result);
+    // await client.end();
     return {
       statusCode: 200,
       body: JSON.stringify({
