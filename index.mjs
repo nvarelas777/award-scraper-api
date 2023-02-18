5467c86bce343d378c3dd4cfac0cf9cbe8338b13
DESKTOP-942RFIF\HBVik <takethisjaguar@gmail.com>
2023-02-18 14:52:34 -0800

    Add variable test

--- index.mjs
+++ index.mjs
@@ -1,3 +1,7 @@
+import pkg from "pg";
+const { Client } = pkg;
+const connectionString = "";
+
 export const lambdaHandler = async (event, context) => {
   if (!event.queryStringParameters?.foo) {
     return {
@@ -8,6 +12,14 @@ export const lambdaHandler = async (event, context) => {
     };
   }
   try {
+    const client = new Client(connectionString);
+    await client.connect();
+    var result = await client.query("select * from testtable t");
+    result.rows.forEach((x) => {
+      console.log(x);
+    });
+    //console.log(result);
+    await client.end();
     return {
       statusCode: 200,
       body: JSON.stringify({
