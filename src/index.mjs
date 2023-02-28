3f068c3a0790a6cbb099c9ea59ff01e0b880be9f
DESKTOP-942RFIF\HBVik <takethisjaguar@gmail.com>
2023-02-28 05:23:20 -0800

    Add layers

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,8 @@
 import pkg from "pg";
 const { Client } = pkg;
 const connectionString = process.env.ConnectionString;
+import puppeteer from "puppeteer-core";
+import Chromium from "@sparticuz/chromium";
 
 export const lambdaHandler = async (event, context) => {
   if (!event.queryStringParameters?.foo) {
@@ -12,17 +14,32 @@ export const lambdaHandler = async (event, context) => {
     };
   }
   try {
-    const client = new Client(connectionString);
-    await client.connect();
-    var result = await client.query("select * from testtable t");
-    result.rows.forEach((x) => {
-      console.log(x);
+    const browser = await puppeteer.launch({
+      args: Chromium.args,
+      defaultViewport: Chromium.defaultViewport,
+      ignoreHTTPSErrors: true,
+      headless: false,
+      executablePath: await Chromium.executablePath(),
     });
-    await client.end();
+    const page = await browser.newPage();
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
+    );
+    await page.goto("https://google.com");
+    const pageTitle = await page.title();
+    const content = await page.content();
+    console.log(pageTitle);
+    // const client = new Client(connectionString);
+    // await client.connect();
+    // var result = await client.query("select * from testtable t");
+    // result.rows.forEach((x) => {
+    //   console.log(x);
+    // });
+    // await client.end();
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: result.rows,
+        message: content,
       }),
     };
   } catch (err) {
